17d6964f94a0dab5b35aaa1c5cfd851bb744b58b
Lv100Luca <luca@trenz.de>
2023-06-21 23:47:49 +0200

    normalized default state

--- src/stores/userDataStore.ts
+++ src/stores/userDataStore.ts
@@ -1,11 +1,12 @@
 import {ref} from 'vue'
 import {defineStore} from 'pinia'
 import APIService from "@/API/APIService";
-
+import {Role} from "@/components/models/Role";
+// todo move login logic here
 export const useUserDataStore = defineStore('UserDataStore', () => {
     const username = ref("Obama Prism");
     const id = ref(-1);
-    const role = ref(0);
+    const role = ref(Role.none);
     const isAuthenticated = ref(false);
     const token = ref<String>("");
 
