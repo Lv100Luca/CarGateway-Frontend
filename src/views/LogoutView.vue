b91de3dd0b898783f7a9a05c6db44454eb4afbda
Lv100Luca <luca@trenz.de>
2023-06-21 23:47:17 +0200

    added functionality to LogoutView.vue

--- src/views/LogoutView.vue
+++ src/views/LogoutView.vue
@@ -1,5 +1,32 @@
 <script setup lang="ts">
+import {useUserDataStore} from "@/stores/userDataStore";
+import {getHighestRole, Role} from "@/components/models/Role";
+import {useRouter} from "vue-router";
+import {onMounted} from "vue";
 
+const userDataStore = useUserDataStore();
+const router = useRouter();
+
+onMounted(async () => {
+  if (userDataStore.isAuthenticated) {
+    await logout();
+    console.log("Logged Out User")
+  }
+})
+
+async function resetState() {
+  userDataStore.username = "";
+  userDataStore.id = -1;
+  userDataStore.role = Role.none;
+  userDataStore.isAuthenticated = false;
+  userDataStore.token = "";
+}
+
+async function logout() {
+
+  await resetState();
+  await router.push("/")
+}
 </script>
 
 <template>
