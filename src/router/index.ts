4ea1a8d9f8141e62fe625642b2f0a87a4b097185
Lv100Luca <luca@trenz.de>
2023-06-21 23:10:33 +0200

    meta data to routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,39 +1,96 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
 import AdminView from "@/views/AdminView.vue";
 import AccountView from "@/views/AccountView.vue";
 import LogoutView from "@/views/LogoutView.vue";
+import {Role} from "@/components/models/Role";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //     path: '/welcome',
+    //     name: 'welcome',
+    //     component: HomeView,
+    //     meta: {
+    //         minimumRole: Role.none,
+    //         onlyGuest: true
+    //     }
+    //
+    // }, todo implkement later maybe???
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        minimumRole: Role.none,
+        onlyGuest: false,
+      }
     },
     {
       path: '/login',
       name: 'login',
-      component: LoginView
+      component: LoginView,
+      meta: {
+        minimumRole: Role.none,
+        onlyGuest: true
+      }
     },
     {
       path: '/logout',
       name: 'logout',
-      component: LogoutView
+      component: LogoutView,
+      meta: {
+        minimumRole: Role.user,
+        onlyGuest: false
+      }
     },
     {
       path: '/account',
       name: 'account',
-      component: AccountView
+      component: AccountView,
+      meta: {
+        minimumRole: Role.user,
+        onlyGuest: false
+      }
     },
     {
       path: '/admin',
       name: 'admin',
-      component: AdminView
+      component: AdminView,
+      meta: {
+        minimumRole: Role.admin,
+        onlyGuest: false
+      }
     },
   ]
-})
+});
+
+router.beforeEach(async (to, from) => {
+  const userData = useUserDataStore();
+  const role = userData.role;
+  const isAuth = userData.isAuthenticated;
+
+  if (to.meta.minimumRole > role && !isAuth) {
+    console.log("Blocked Route to: " + to.path)
+
+    return {name: "login", query: {return: to.path}} // routes user to Login
+  }
+
+});
 
 export default router
+
+
+export {};
+
+import "vue-router";
+import {useUserDataStore} from "@/stores/userDataStore";
+
+declare module "vue-router" {
+  interface RouteMeta {
+    onlyGuest: boolean;
+    minimumRole: Role;
+  }
+}
