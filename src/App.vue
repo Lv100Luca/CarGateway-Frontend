1b60931dce931e488a0a1d34ebd5412a81a61565
Lv100Luca <luca@trenz.de>
2023-06-22 23:15:35 +0200

    pulls Token from localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 
 import NavBar from "@/components/NavBar.vue";
+import {onMounted} from "vue";
+import {useUserDataStore} from "@/stores/userDataStore";
+
+const userData = useUserDataStore();
+onMounted(async () => await userData.fetchSelf());
 </script>
 
 <template>
