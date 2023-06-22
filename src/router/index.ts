84a011a33f399b37a351db5d8fa4ee36e59db7ab
Lv100Luca <luca@trenz.de>
2023-06-23 00:27:11 +0200

    Added fetchSelf Before RouterGuards

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,76 +7,77 @@ import LogoutView from "@/views/LogoutView.vue";
 import {Role} from "@/components/models/Role";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // {
-    //     path: '/welcome',
-    //     name: 'welcome',
-    //     component: HomeView,
-    //     meta: {
-    //         minimumRole: Role.none,
-    //         onlyGuest: true
-    //     }
-    //
-    // }, todo implkement later maybe???
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      meta: {
-        minimumRole: Role.none,
-        onlyGuest: false,
-      }
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
-      meta: {
-        minimumRole: Role.none,
-        onlyGuest: true
-      }
-    },
-    {
-      path: '/logout',
-      name: 'logout',
-      component: LogoutView,
-      meta: {
-        minimumRole: Role.user,
-        onlyGuest: false
-      }
-    },
-    {
-      path: '/account',
-      name: 'account',
-      component: AccountView,
-      meta: {
-        minimumRole: Role.user,
-        onlyGuest: false
-      }
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: AdminView,
-      meta: {
-        minimumRole: Role.admin,
-        onlyGuest: false
-      }
-    },
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        // {
+        //     path: '/welcome',
+        //     name: 'welcome',
+        //     component: HomeView,
+        //     meta: {
+        //         minimumRole: Role.none,
+        //         onlyGuest: true
+        //     }
+        //
+        // }, todo implkement later maybe???
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView,
+            meta: {
+                minimumRole: Role.none,
+                onlyGuest: false,
+            }
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView,
+            meta: {
+                minimumRole: Role.none,
+                onlyGuest: true
+            }
+        },
+        {
+            path: '/logout',
+            name: 'logout',
+            component: LogoutView,
+            meta: {
+                minimumRole: Role.user,
+                onlyGuest: false
+            }
+        },
+        {
+            path: '/account',
+            name: 'account',
+            component: AccountView,
+            meta: {
+                minimumRole: Role.user,
+                onlyGuest: false
+            }
+        },
+        {
+            path: '/admin',
+            name: 'admin',
+            component: AdminView,
+            meta: {
+                minimumRole: Role.admin,
+                onlyGuest: false
+            }
+        },
+    ]
 });
 
 router.beforeEach(async (to, from) => {
-  const userData = useUserDataStore();
-  const role = userData.role;
-  const isAuth = userData.hasUser;
+    const userData = useUserDataStore();
+    await userData.fetchSelf();
+    const role = userData.role;
+    const isAuth = userData.hasUser;
 
-  if (to.meta.minimumRole > role && !isAuth) {
-    console.log("Blocked Route to: " + to.path)
+    if (to.meta.minimumRole > role && !isAuth) {
+        console.log("Blocked Route to: " + to.path)
 
-    return {name: "login", query: {return: to.path}} // routes user to Login
-  }
+        return {name: "login", query: {return: to.path}} // routes user to Login
+    }
 
 });
 
@@ -87,10 +88,11 @@ export {};
 
 import "vue-router";
 import {useUserDataStore} from "@/stores/userDataStore";
+import APIClient from "@/API/APIClient";
 
 declare module "vue-router" {
-  interface RouteMeta {
-    onlyGuest: boolean;
-    minimumRole: Role;
-  }
+    interface RouteMeta {
+        onlyGuest: boolean;
+        minimumRole: Role;
+    }
 }
