3b6ee808770ea2e9edfdd2807a6f843f5d4620e8
Lv100Luca <luca@trenz.de>
2023-06-25 22:07:08 +0200

    Created new componetens

---
+++ src/components/PageSelector.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+
+import {ref} from "vue";
+
+const emits = defineEmits(["PageNr"]);
+const props = defineProps({
+  pageLimit: ref<number>,
+})
+const pageNr = ref(1);
+
+function increment() {
+  pageNr.value++;
+  emits('PageNr', pageNr.value);
+}
+
+function decrement() {
+  pageNr.value--;
+  emits('PageNr', pageNr.value);
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <input class="button" :disabled="(pageNr <= 0)" type="button" value="-" @click="decrement()">
+    <input class="page-nr" disabled="disabled" type="text" :value="pageNr + 1">
+    <input class="button" :disabled="(pageNr >= pageLimit)" type="button" value="+" @click="increment()">
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: row;
+//width: 100%;
+}
+.button {
+  width: 20%;
+}
+.page-nr {
+  width: 60%;
+}
+.wrapper > * {
+}
+</style>
---
+++ src/components/UserDisplayItem.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import type UserResponseDTO from "@/DTO/UserResponseDTO";
+import {getHighestRole} from "./models/Role";
+
+const props = defineProps({
+  user: UserResponseDTO
+})
+// const emits = defineEmits(['userID']);
+</script>
+<template>
+  <div class="wrapper" @click="user!.id">
+    <div class="left">
+      <h1>{{user!.id}}</h1>
+    </div>
+    <a class="v-border"/>
+    <div class="right">
+      <div class="role-bottom">
+        <h3>{{user!.username}}</h3>
+        <h3>{{getHighestRole(user!.rollen)}}</h3>
+      </div>
+      <a class="h-border"/>
+      <div class="name-top">
+        <h3>{{user!.vorname}}</h3>
+        <h3>{{user!.nachname}}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.wrapper {
+  border: gray 2px solid;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+}
+
+.left {
+  display: flex;
+  justify-content: center;
+  width: 25%;
+}
+
+.right {
+  width: 75%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.name-top, .role-bottom {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+
+.v-border {
+  border-right: gray 1px solid;
+}
+
+.h-border {
+  border-top: gray 1px solid;
+}
+
+</style>
