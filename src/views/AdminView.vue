a3042e0f853763098d45d78b4503a79cc83572c1
Lv100Luca <luca@trenz.de>
2023-06-26 00:32:50 +0200

    added UserDisplayItem.vue and PageSelector.vue to AdminView.vue

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,25 +1,73 @@
 <script setup lang="ts">
 import {useUserDataStore} from "@/stores/userDataStore";
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 import type UserResponseDTO from "@/DTO/UserResponseDTO";
 import APIClient from "@/API/APIClient";
 import type UserListResponseDTO from "@/DTO/UserListResponseDTO";
+import UserDisplayItem from "@/components/UserDisplayItem.vue";
+import PageSelector from "@/components/PageSelector.vue";
 
 const userData = useUserDataStore();
 
 const users = ref<UserResponseDTO[]>();
+const selectedUser = ref();
+
+const pageNr = ref(0);
+const pageSize = 5;
+const totalNrOfElements = ref(5);
+const pageLimit = ref((totalNrOfElements.value / pageSize) - 1);
+
 
 onMounted(async () => {
-  const response = await APIClient.getRequest<UserListResponseDTO>("/user/getall?pagenr=0&pagesize=100", true);
-  users.value = response.content;
+  await loadUsers();
 })
+watch(pageNr, () => {
+  loadUsers();
+})
+
+async function loadUsers() {
+  const response = await APIClient.getRequest<UserListResponseDTO>("/user/getall?pagenr=" + pageNr.value + "&pagesize=" + pageSize, true);
+  users.value = response.content;
+  totalNrOfElements.value = response.nrOfTotalElements;
+  selectedUser.value = null;
+}
+
 </script>
 
 <template>
-  <h1>ADMIN Panel</h1>
-  <pre>{{users}}</pre>
-  <h2></h2>
+  <div class="admin-wrapper">
+    <h1>ADMIN Panel</h1>
+    <pre>Selected User: {{ selectedUser }}</pre>
+    <PageSelector class="selector" :page-limit="pageLimit" @PageNr="args => pageNr = args"></PageSelector>
+    <div class="users">
+      <UserDisplayItem v-for="user in users" v-bind:user="user" @userID="userID => selectedUser = userID"
+                       :class="{ 'highlighted': selectedUser === user.id }"/>
+    </div>
+  </div>
 </template>
-<style>
+<style scoped>
+.admin-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: .5rem;
+}
 
+
+.selector {
+  display: flex;
+  height: 2rem;
+  width: 50%;
+}
+.users {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 50%;
+}
+
+.highlighted {
+  /* Add your highlight style here */
+  background-color: yellowgreen;
+}
 </style>
