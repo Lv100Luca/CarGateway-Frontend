719bbdbab78e2cbb02e000135e388b89bcf8b625
Luca Diegel <Luca@trenz.de>
2023-06-29 10:15:11 +0200

    changed name to .vue

---
+++ src/views/Register.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+const vorname = ref("");
+const nachname = ref("");
+const username = ref("");
+const password = ref("");
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <form class="register-from">
+      <h1>Register</h1>
+      <div class="name">
+        <label>Vorname:
+          <input type="text" v-model="vorname" placeholder="vorname">
+        </label>
+        <label>Nachname:
+          <input type="text" v-model="nachname" placeholder="nachname">
+        </label>
+      </div>
+      <label class="username">
+        Username:
+        <input type="text" v-model="username" placeholder="username">
+      </label>
+      <label class="password">
+        <input type="password" v-model="password" placeholder="password">
+      </label>
+    </form>
+  </div>
+</template>
+
+<style>
+
+</style>
