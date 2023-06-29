544e83e6385db608bfb767c81a0ea039b7dd03bc
Luca Diegel <Luca@trenz.de>
2023-06-29 12:51:14 +0200

    added isComplete

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,12 +1,44 @@
 <script setup lang="ts">
 
-import {ref} from "vue";
+import {computed, ref} from "vue";
+// @ts-ignore
+import APIClient from "@/API/APIClient";
+// @ts-ignore
+import type UserRegisterDTO from "@/DTO/UserRegisterDTO";
+import {useUserDataStore} from "../stores/userDataStore";
 
-const vorname = ref("");
-const nachname = ref("");
-const username = ref("");
-const password = ref("");
+const userDataStore = useUserDataStore();
+const vorname = ref("luca");
+const nachname = ref("diegel");
+const username = ref("luca");
+const password = ref("admin");
 
+
+const isComplete = computed(() => {
+  return (vorname.value !== "" && nachname.value !== "" && username.value !== "" && password.value !== "");
+
+})
+
+async function register() {
+  console.log("submitting");
+  try {
+    const response = await APIClient.postRequest<UserRegisterDTO>("/user/register", true, {
+      "vorname": vorname.value,
+      "nachname": nachname.value,
+      "username": username.value,
+      "password": password.value
+    });
+  } catch (e) {
+    console.log("error in Register");
+    vorname.value = "";
+    nachname.value = "";
+    username.value = "";
+    password.value = "";
+    console.log(e);
+  }
+  console.log("Register OK");
+  // await userDataStore.login(username.value, password.value);
+}
 </script>
 
 <template>
@@ -14,24 +46,48 @@ const password = ref("");
     <form class="register-from">
       <h1>Register</h1>
       <div class="name">
-        <label>Vorname:
-          <input type="text" v-model="vorname" placeholder="vorname">
+        <label style="display: flex; flex-direction: column">Vorname:
+          <input required type="text" v-model="vorname" placeholder="vorname">
         </label>
-        <label>Nachname:
-          <input type="text" v-model="nachname" placeholder="nachname">
+        <label style="display: flex; flex-direction: column">Nachname:
+          <input required type="text" v-model="nachname" placeholder="nachname">
         </label>
       </div>
-      <label class="username">
+      <label class="username" style="display: flex; flex-direction: column">
         Username:
-        <input type="text" v-model="username" placeholder="username">
+        <input required type="text" v-model="username" placeholder="username">
       </label>
-      <label class="password">
-        <input type="password" v-model="password" placeholder="password">
+      <label class="password" style="display: flex; flex-direction: column">
+        Password:
+        <input required type="password" v-model="password" placeholder="password">
       </label>
+      <input :disabled="!isComplete" type="button" @click="register" value="Register">
     </form>
   </div>
 </template>
 
-<style>
+<style scoped>
+.wrapper {
+  height: calc(100dvh - var(--navbar-height));
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+}
 
+.register-from {
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+}
+
+.name {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 1rem;
+}
+
+.name > label {
+  width: 50%;
+}
 </style>
